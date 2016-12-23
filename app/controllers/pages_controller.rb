bcea92b9f09f0d24189a54ad152435f80f497152
Constant Desfosse <desfosse.constant@gmail.com>
2016-12-23 20:58:07 +0100

    designing home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,19 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :projets, :prestations, :philosophie, :contact ]
 
   def home
   end
+
+  def projets
+  end
+
+  def prestations
+  end
+
+  def philosophie
+  end
+
+  def contact
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  get 'projets' => 'pages#projets'
+  get 'prestations' => 'pages#prestations'
+  get 'philosophie' => 'pages#philosophie'
+  get 'contact' => 'pages#contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
