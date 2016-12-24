8fd2c47e493072cb370f4da4ac22485d19e296f0
Constant Desfosse <desfosse.constant@gmail.com>
2016-12-24 16:15:55 +0100

    fxing mailer

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,4 @@
 class Contact < MailForm::Base
-  attribute :sujet      :validate => true
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
@@ -9,7 +8,7 @@ class Contact < MailForm::Base
   # in ActionMailer accepts.
   def headers
     {
-      :subject => %("#{sujet}),
+      :subject => "My Contact Form",
       :to => "desfosse.constant@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
